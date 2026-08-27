49c3632a35a3b4179fef875b235eb3eb059ea1e7
Aneesh Pallapolu <yaagnik.p@gmail.com>
2026-08-27 17:18:44 -0400

    refactor: remove favicon and related metadata from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Temporary Name | Screenplay-to-3D Pre-Visualization",
   description: "Turn screenplay pages into interactive 3D scenes in under 60 seconds.",
-  icons: {
-    icon: "/favicon.svg",
-    shortcut: "/favicon.svg",
-  },
 };
 
 export default function RootLayout({
